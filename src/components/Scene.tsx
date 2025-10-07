43cd954e22fbbd2207dc56ad447fccf54ce370d5
DoctorTone <tony@drt-software.com>
2025-10-07 23:38:39 +0100

    Added 2 ghosts to scene

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -1,11 +1,12 @@
 import Pumpkin from "./Pumpkin";
-import Ghost from "./Ghost";
+import { Ghost } from "./Ghost";
 
 const Scene = () => {
   return (
     <>
-      <Pumpkin />;
-      <Ghost />
+      <Pumpkin />
+      <Ghost scale={0.05} position={[-0.5, 0, 0]} />
+      <Ghost scale={0.05} position={[0.5, 0, 0]} />
     </>
   );
 };
