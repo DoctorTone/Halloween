05db3c9b22a1d609fadb885f41f0c7b962fa1223
DoctorTone <tony@drt-software.com>
2025-10-21 14:33:26 +0100

    Adjust camera position and pumpkin rotation

--- src/components/Pumpkin.tsx
+++ src/components/Pumpkin.tsx
@@ -3,7 +3,14 @@ import { useGLTF } from "@react-three/drei";
 const Pumpkin = () => {
   const { scene } = useGLTF("./models/pumpkin.glb");
 
-  return <primitive position-y={-0.05} scale={1.2} object={scene} />;
+  return (
+    <primitive
+      position-y={-0.05}
+      scale={1.2}
+      rotation-y={-Math.PI / 9}
+      object={scene}
+    />
+  );
 };
 
 export default Pumpkin;
