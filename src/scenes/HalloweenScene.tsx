b927bdcfcf571edf64ae3414522202b02fb44a75
DoctorTone <tony@drt-software.com>
2025-10-23 16:30:37 +0100

    Added money text

--- src/scenes/HalloweenScene.tsx
+++ src/scenes/HalloweenScene.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { useGLTF, Stage } from "@react-three/drei";
+import { useGLTF, Stage, Text } from "@react-three/drei";
 import { useThree } from "@react-three/fiber";
 import { Color } from "three";
 import ResponsiveCamera from "../components/ResponsiveCamera";
@@ -28,13 +28,22 @@ const HalloweenScene = () => {
       >
         <primitive
           scale={0.06}
-          position={[22.5, -4.75, 11]}
+          position={[22.5, -4, 11]}
           object={gltfs[0].scene}
           rotation-y={-Math.PI / 8}
         />
         <primitive scale={0.4} position={[-30, 0, 0]} object={gltfs[1].scene} />
+        <Text position={[-30, 1.5, 0]} fontSize={1} color="orange">
+          $300K
+        </Text>
         <primitive scale={3.5} position={[0, 0, 0]} object={gltfs[2].scene} />
+        <Text position={[0, 2, 0]} fontSize={1} color="orange">
+          $47M
+        </Text>
         <primitive scale={10} position={[30, 0, 0]} object={gltfs[3].scene} />
+        <Text position={[30, 6, 0]} fontSize={1} color="orange">
+          {`>$150M`}
+        </Text>
       </Stage>
     </>
   );
