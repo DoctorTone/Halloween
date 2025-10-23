c6a85de05fa2f838454c24cdf60be1c010fe9865
DoctorTone <tony@drt-software.com>
2025-10-23 12:23:16 +0100

    Added Michael Myers model

--- src/scenes/HalloweenScene.tsx
+++ src/scenes/HalloweenScene.tsx
@@ -1,7 +1,32 @@
-import { Box } from "@react-three/drei";
+import { useEffect } from "react";
+import { useGLTF, Stage } from "@react-three/drei";
+import { useThree } from "@react-three/fiber";
+import { Color } from "three";
+import ResponsiveCamera from "../components/ResponsiveCamera";
 
 const HalloweenScene = () => {
-  return <Box />;
+  const gltf = useGLTF("./models/michael_myers.glb");
+  const { scene } = useThree();
+
+  useEffect(() => {
+    scene.background = new Color().setHex(0x2d2d2d);
+  }, []);
+
+  return (
+    <>
+      <ResponsiveCamera scene="Halloween" />
+      <Stage
+        adjustCamera={2}
+        center={{ disable: false }}
+        environment={"night"}
+        shadows={"contact"}
+      >
+        <primitive object={gltf.scene} />
+      </Stage>
+    </>
+  );
 };
 
 export default HalloweenScene;
+
+useGLTF.preload("./models/michael_myers.glb");
