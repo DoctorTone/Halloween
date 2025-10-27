518b0d76fd3fdf8fc8d72ea20599d84d2a16feb4
DoctorTone <tony@drt-software.com>
2025-10-27 15:57:30 +0000

    Toggle audio via UI

--- src/UI/AudioToggle.tsx
+++ src/UI/AudioToggle.tsx
@@ -1,12 +1,20 @@
 import Switch from "@mui/material/Switch";
 import MusicNoteIcon from "@mui/icons-material/MusicNote";
 import Box from "@mui/material/Box";
+import type { ChangeEvent } from "react";
+import useStore from "../state/store";
 
 const AudioToggle = () => {
+  const setAudioEnabled = useStore((state) => state.setAudioEnabled);
+
+  const onToggleAudio = (event: ChangeEvent<HTMLInputElement>) => {
+    setAudioEnabled(event.target.checked);
+  };
+
   return (
     <div id="audio" className="panel">
       <Box sx={{ display: "flex", alignItems: "center" }}>
-        <Switch defaultChecked />
+        <Switch defaultChecked onChange={onToggleAudio} />
         <MusicNoteIcon color="primary" />
       </Box>
     </div>
